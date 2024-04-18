aa98ba1434913a46958bcda2a7fbadc4894fabc6
Nicroxio <Ferdi@Nicroxio.co.uk>
2024-04-18 11:26:10 +0100

    Add Docker to .gitignore and update Cargo.toml with serde dependency

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use maxminddb;
 use rouille::Response;
+use serde;
 use std::{
     net::{IpAddr, Ipv4Addr},
     str::FromStr,
 };
 
-#[derive(Debug)]
+#[derive(Debug, serde::Serialize, Default)]
 struct CityData {
+    ip: String,
     city_name: String,
     country_name: String,
     is_in_european_union: bool,
@@ -14,18 +16,27 @@ struct CityData {
 }
 
 fn main() {
-    rouille::start_server("127.0.0.1:8080", move |request| {
+    println!("Starting");
+    rouille::start_server("0.0.0.0:8080", move |request| {
         let forwarded_for = request.header("X-Forwarded-For");
+
         let ip: IpAddr = std::net::IpAddr::V4(Ipv4Addr::from_str(forwarded_for.unwrap()).unwrap());
-        let data = lookup_ip(ip);
+
+        let data = match lookup_ip(ip) {
+            Ok(data) => data,
+            Err(e) => {
+                eprintln!("Error:{}", e);
+                CityData::default()
+            }
+        };
         println!("{}", ip);
-        println!("{:?}", forwarded_for);
-        Response::text(format!("{:?}", data))
+        Response::json(&data)
     });
 }
 
-fn parse_data(lookup: maxminddb::geoip2::City) -> CityData {
+fn parse_data(lookup: maxminddb::geoip2::City, ip: IpAddr) -> CityData {
     let mut data = CityData {
+        ip: ip.to_string(),
         city_name: String::new(),
         country_name: String::new(),
         is_in_european_union: false,
@@ -56,9 +67,7 @@ fn parse_data(lookup: maxminddb::geoip2::City) -> CityData {
 }
 
 fn lookup_ip(ip: IpAddr) -> Result<CityData, maxminddb::MaxMindDBError> {
-    let reader = maxminddb::Reader::open_readfile(
-        "/home/Nic/computers/code/website/shronk_ip/dbip-city-lite-2023-10.mmdb",
-    )?;
+    let reader = maxminddb::Reader::open_readfile("/app/dbip-city-lite-2023-10.mmdb")?;
     let city: maxminddb::geoip2::City = reader.lookup(ip)?;
-    Ok(parse_data(city))
+    Ok(parse_data(city, ip))
 }
